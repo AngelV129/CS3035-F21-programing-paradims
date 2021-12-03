a4de154908abbafd5fb9bdb5083a4cf477c76ec1
Angel Villalobos <angelvillalobos1995@gmail.com>
2021-12-02 21:44:37 -0800

    extra credit

--- week-14/lab13/Objects.hs
+++ week-14/lab13/Objects.hs
@@ -5,7 +5,18 @@ module Objects
 ) where  
 
 
+data Object = Sphere Float -- radius
+            | Cube Float  
+            | Cuboid Float Float Float 
+
+
 volume :: Object -> Float  
+volume (Sphere r) = (4/3) * pi * (r ^ 3)
+volume (Cube a) = a ^ 3
+volume (Cuboid l w h) = l * w * h
 
 
-area :: Object -> Float  
+area :: Object -> Float
+area (Sphere r ) = 4 * pi * r ^ 2
+area (Cube a) = 6 * (a^2)
+area (Cuboid l w h) = (2 * l * w) + (2 * l * h) + (2 * h * w)  
--- week-14/lab13/Shapes.hs
+++ week-14/lab13/Shapes.hs
@@ -1,18 +1,26 @@
 module Shapes   
 ( Point(..)
 , Shape(..)
-, area 
+, area
+, perimeter 
 ) where  
 
 data Point = Point Float Float
 
 data Shape = Circle Point Float
             | Rectangle Point Point
+            | RightTriangle Point Point Point
+            
+
 
 
 area :: Shape -> Float
 area (Circle _ r) = pi * r ^ 2
 area (Rectangle (Point x1 y1) (Point x2 y2)) = (abs $ x2 - x1) * (abs $ y2 - y1)
+area (RightTriangle (Point x1 y1) (Point x2 y2) (Point x3 y3)) =   ((abs $ y3 - y1 ) * (abs $ x2 - x1)) / 2          -- 1/2 b*h
 
 
 perimeter :: Shape -> Float  
+perimeter (Circle _ r) = 2 * pi * r
+perimeter (Rectangle (Point x1 y1) (Point x2 y2)) = 2 *  ((abs $ x2 - x1) + (abs $ y2 - y1))
+perimeter (RightTriangle (Point x1 y1) (Point x2 y2) (Point x3 y3)) = ((abs $ y3 - y1 ) + (abs $ x2 - x1)) + sqrt (((abs $ y3 - y1 ) ^ 2) + ((abs $ x2 - x1) ^ 2 ) ) -- a + b + squrt (a ^ 2  + b^2)
